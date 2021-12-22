280bdfe8bb489f2530e0547685107e0e53312ac7
Tsutomu Kawamura <cognitom@gmail.com>
2021-12-22 19:53:53 +0900

    長音記号キーの追加

--- firmware/keymaps/default/keymap.c
+++ firmware/keymaps/default/keymap.c
@@ -478,6 +478,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         return false;
       }
       break;
+    case KC_QUOT: // ー
+      if (is_kana && shifted && pressed) { 
+        tap_code(KC_MINS);
+        return false;
+      }
+      break;
   }
 
   return true;
