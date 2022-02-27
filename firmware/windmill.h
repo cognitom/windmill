a3ae56a3ec2a64e3b2060ef9e89e0145365ea3e0
Tsutomu Kawamura <cognitom@gmail.com>
2022-02-27 20:45:10 +0900

    コピーライトの直し忘れを修正

--- firmware/windmill.h
+++ firmware/windmill.h
@@ -1,4 +1,4 @@
-/* Copyright 2020 Boardsource
+/* Copyright 2021 Tsutomu Kawamura
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
